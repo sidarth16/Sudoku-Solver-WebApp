ee694fa9d55dc1a4535898d5e8944e072dc1b0d9
S.Sidarth <38394431+sidarth16@users.noreply.github.com>
2020-07-20 15:29:23 +0530

    Main app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from pickle import dump
 
 from tensorflow import keras
 import numpy as np
-from flask import Flask, request, jsonify, render_template,redirect,flash,send_file,session
+from flask import Flask, request, jsonify, render_template,redirect,flash,send_file
 
 import os
 import urllib.request
@@ -24,19 +24,8 @@ from sudoku_main import sudoku_crop_solve_save
 
 import cv2
 
-import numpy as np
-from json import JSONEncoder
-import json
 
-class NumpyArrayEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return JSONEncoder.default(self, obj)
-
-
-solution, existing_numbers ,sudoku , cropped_sudoku = 0,0,0,False
-raw_image=0
+solution, existing_numbers ,sudoku , cropped_sudoku = 0,0,0,0
 raw_img_count=0
 raw_img_path=r"static\img\sudoku\raw_sudoku_{count}.jpg".format(count=str(raw_img_count))
 crop_img_path=r"static\img\sudoku\cropped_sudoku_{count}.jpg".format(count=str(raw_img_count))
@@ -44,19 +33,11 @@ img_count = 0
 img_name = "solved_cropped{no}_sudoku_{count}.jpg".format(no=raw_img_count, count=img_count)
 img_path = r"static\img\sudoku\{}".format(img_name)
 active_num=""
-cropped_sudoku_url,solved_sudoku_url= 0,0
 def sudoku_ready():
     global solution , existing_numbers , sudoku ,cropped_sudoku ,raw_img_count , img_count , active_num
-    global cropped_sudoku_url,solved_sudoku_url
-    
-    encodedNumpyData = session["raw_image"]
-    decodedArrays = json.loads(encodedNumpyData)
-    finalNumpyArrayOne = np.asarray(decodedArrays["raw_image"])
-    raw_image = finalNumpyArrayOne
-
-    solution , existing_numbers , sudoku , cropped_sudoku , cropped_sudoku_url,solved_sudoku_url= sudoku_crop_solve_save(raw_image , raw_img_count , required_num_in_sol="0")
-    print(solution)
-    if(sudoku) :
+    solution , existing_numbers , sudoku ,cropped_sudoku = sudoku_crop_solve_save(raw_img_count , required_num_in_sol="0")
+    print(sudoku)
+    if sudoku :
         img_count=0
         active_num=""
         print("sudoku_ready")
@@ -67,44 +48,29 @@ def sudoku_ready():
 def sudoku_filter_sol(req_num):
     if (req_num=="All"):
         req_num="123456789"
-    global img_count , img_path , img_name , solved_sudoku_url
+    global img_count , img_path , img_name
     img_count=req_num
-    
-    encodedNumpyData = session["raw_image"]
-    decodedArrays = json.loads(encodedNumpyData)
-    finalNumpyArrayOne = np.asarray(decodedArrays["raw_image"])
-    raw_image = finalNumpyArrayOne
-    solution , existing_numbers , sudoku , cropped_sudoku , cropped_sudoku_url,solved_sudoku_url= sudoku_crop_solve_save(raw_image , raw_img_count , required_num_in_sol=req_num)
-#     img_name = "solved_cropped{no}_sudoku_{count}.jpg".format(no=raw_img_count, count=img_count)
-#     img_path = r"static\img\sudoku\{}".format(img_name)
+    img_name = "solved_cropped{no}_sudoku_{count}.jpg".format(no=raw_img_count, count=img_count)
+    img_path = r"static\img\sudoku\{}".format(img_name)
 
-#     if(cropped_sudoku.all()==False):
-#         return False
-#         #return redirect(url_for("upload"))
-    
-#     cropped_sudoku_copy = cropped_sudoku.copy() 
-#     sudoku.write_solution(cropped_sudoku_copy, solution, ignore=existing_numbers ,required_num_in_sol =req_num)
-#     solved_cropped_sudoku_copy = cv2.resize(cropped_sudoku_copy , (450,450))
-    
-#     from sudoku_main import get_img_url
-#     solved_sudoku_url = get_img_url(solved_cropped_sudoku_copy)
-    
-#     cv2.imwrite( img_path, solved_cropped_sudoku_copy )
+    cropped_sudoku_copy = cropped_sudoku.copy() 
+    sudoku.write_solution(cropped_sudoku_copy, solution, ignore=existing_numbers ,required_num_in_sol =req_num)
+    cropped_sudoku_copy = cv2.resize(cropped_sudoku_copy , (450,450))
+    cv2.imwrite( img_path, cropped_sudoku_copy )
     return True
 
 
 #from neural_model import NeuralModel
 #NeuralModel.instance()
 app = Flask(__name__)
-app.secret_key = 'A1B2C3D4E4F5G6HI78JKLMNOPQRST1324354657687980'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 @app.route("/")
 @app.route("/home")
 def home():
-#     mydir = r"static/img/sudoku"
-#     if (len(os.listdir(mydir))>0):
-#         for f in os.listdir(mydir):
-#             os.remove(os.path.join(mydir, f))
+    mydir = r"static/img/sudoku"
+    if (len(os.listdir(mydir))>0):
+        for f in os.listdir(mydir):
+            os.remove(os.path.join(mydir, f))
     return render_template('index.html' , page="home")
 
 @app.route("/confirm" , methods=["GET" , "POST"])
@@ -117,22 +83,9 @@ def confirm():
         img = request.files['sudoku_raw_img']
         if(img.filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS) :
             img.save(raw_img_path)
-
-            #image = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            from PIL import Image
-            # image = Image.open(img.stream)
-            # image = cv2.imdecode(np.fromstring(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            image = np.asarray(Image.open(img.stream))
-            global raw_image
-            raw_image = image
-            encodedNumpyData=json.dumps(raw_image,  cls=NumpyArrayEncoder)
-            session["raw_image"] = encodedNumpyData
-            print("image.shape : ",image.shape)
-            
-            #image_url = 
             if(sudoku_ready()):
                 print("sudoku ready")
-                return render_template('confirm.html',page="confirm", crop_img_path=cropped_sudoku_url)
+                return render_template('confirm.html',page="confirm", crop_img_path=crop_img_path)
                 
             else:
                 print("Sudoku Not Ready")
@@ -148,17 +101,16 @@ def confirm():
 
 @app.route("/confirm_again")
 def confirm_again():
-    global raw_img_count , crop_img_path , raw_image
+    global raw_img_count , crop_img_path
     print("Inside (confirm_again) raw_img_count = ",raw_img_count)
     raw_img_count=raw_img_count + 1
     
     raw_img_path=r"static\img\sudoku\raw_sudoku_{count}.jpg".format(count=str(raw_img_count))
     cv2.imwrite(raw_img_path , cropped_sudoku)
-    raw_image = cropped_sudoku
     crop_img_path=r"static\img\sudoku\cropped_sudoku_{count}.jpg".format(count=str(raw_img_count))
     if(sudoku_ready()):
         print("sudoku ready")
-        return render_template('confirm_again.html',page="confirm", crop_img_path=cropped_sudoku_url)
+        return render_template('confirm_again.html',page="confirm", crop_img_path=crop_img_path)
         
     else:
         print("Sudoku Not Ready")
@@ -183,16 +135,15 @@ def result():
     img_name = "solved_cropped{no}_sudoku_{count}.jpg".format(no=raw_img_count, count=img_count)
     img_path = r"static\img\sudoku\{}".format(img_name)
 
-    return render_template('result.html' , img_path=solved_sudoku_url , active_num=active_num ,page="result")
+    return render_template('result.html' , img_path=img_path , active_num=active_num ,page="result")
 
 
 @app.route("/result/<string:num>" )
 def result_filter(num="All"):
     global raw_img_count
     print("Inside (result_filter) raw_img_count = ",raw_img_count)
-    if(solution==0):
-        print("solution=0 and num = ",num)
-        #return redirect(url_for('upload'))
+    if solution==0:
+        sudoku_ready()
     print("in result_filter route")
     global active_num
     if(num):
@@ -200,9 +151,6 @@ def result_filter(num="All"):
         print("required_num_in_sol = "+num)
         if(sudoku_filter_sol(num)):
             print("solution filtered successfully")
-        else:
-            return redirect(url_for('result/'+num))
-            
     else:
         active_num=""
         sudoku_filter_sol("All")
@@ -216,12 +164,12 @@ def guide():
     return render_template('instructions.html' , page="guide")
 
 if __name__ == "__main__":
-    
-    app.run(debug=True , threaded=False )
-# if app.config["DEBUG"]:
-#     @app.after_request
-#     def after_request(response):
-#         response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-#         response.headers["Expires"] = 0
-#         response.headers["Pragma"] = "no-cache"
-#         return response
+    app.secret_key = 'super secret key'
+    app.run(debug=False , threaded=False )
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
